dd5c59957722b301fa2820bc44bdba7806391129
VASANTHARAN <107398944+KUMARANVASANTH@users.noreply.github.com>
2023-05-15 12:33:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function validate() {
     const nameRegex = /^[a-zA-Z\s]+$/;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const phoneRegex = /^\d{10}$/;
-    const passwordRegex = /^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[a-zA-Z]).{8,}$/;
+    const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/;
     if (!nameRegex.test(name)) {
         alert('Please enter a valid name.');
         return false;
